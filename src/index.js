8418288c50f77f696cd2d4793cdbbd64b9544b00
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-17 17:03:23 +0530

    feat: code restructure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
@@ -48,8 +50,6 @@ camera.position.x = -30;
 camera.position.y = 40;
 camera.position.z = 30;
 camera.lookAt(scene.position);
-
-
 
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
 renderer.render(scene, camera);
