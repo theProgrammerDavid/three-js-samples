7634a7e162e4829978e726f714939a66c25b4da9
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-18 17:14:05 +0530

    [WIP] Webpack Build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import _ from 'lodash';
+import * as THREE from 'three';
+
+const Object = require('Object');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -11,7 +14,7 @@ var axes = new THREE.AxisHelper(20);
 scene.add(axes);
 
 var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
-var planeMaterial = new THREE.MeshBasicMaterial({color:0xcccccc});
+var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
 plane.rotation.x = -0.5 * Math.PI;
@@ -34,17 +37,19 @@ cube.position.z = 0;
 
 scene.add(cube);
 
-var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-var sphereMaterial = new THREE.MeshBasicMaterial({
+var ob1 = new Object();
+
+ob1.addGeometry(new THREE.SphereGeometry(4, 20, 20));
+ob1.addMaterial(new THREE.MeshBasicMaterial({
     color:
         0x7777ff, wireframe: true
-});
-var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+}));
+ob1.createMesh();
 
-sphere.position.x = 20;
-sphere.position.y = 4;
-sphere.position.z = 2;
-scene.add(sphere);
+ob1.setPosition(20,4,2);
+
+
+scene.add(ob1);
 
 camera.position.x = -30;
 camera.position.y = 40;
