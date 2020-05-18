091d4d89fc0b1de29bc66b4aa4b63463e7053ca1
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-18 17:13:44 +0530

    Feat: Object Type

---
+++ src/Object.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+
+class Object extends THREE.Mesh {
+    constructor() {
+        super();
+    }
+    addGeometry(geometry) {
+        this.geometry = geometry;
+    }
+    addMaterial(material) {
+        this.material = material;
+    }
+    createMesh() {
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+    }
+    get getMesh() {
+        return this.mesh;
+    }
+    /**
+     * @param {number} x
+     */
+    setPosition(x, y, z) {
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    }
+}
+export default {Object};
