ce2c2516ee021501a7e4c1c8b18d5efe32dc815e
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-26 01:17:58 +0530

    feat: lighting

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 
-renderer.setClearColor(0xEEEEEE, 1);
+renderer.setClearColor(0x222222, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 var axes = new THREE.AxisHelper(20);
@@ -15,39 +15,38 @@ scene.add(axes);
 
 var plane = new Object();
 plane.addGeometry(new THREE.PlaneGeometry(60, 20, 1, 1));
-plane.addMaterial(new THREE.MeshBasicMaterial({ color: 0xcccccc }) );
-plane.setPosition(15,0,0);
+plane.addMaterial(new THREE.MeshLambertMaterial({color:
+    0xffffff}));
+plane.setPosition(15, 0, 0);
 plane.rotation.x = -0.5 * Math.PI;
 
 scene.add(plane);
-var cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
 
-var cubeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true
-});
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-cube.position.x = -4;
-cube.position.y = 3;
-cube.position.z = 0;
-
+var cube = new Object();
+cube.addGeometry(new THREE.BoxGeometry(4, 4, 4))
+cube.addMaterial(new THREE.MeshLambertMaterial({
+    color:
+        0xff0000
+}))
+cube.createMesh();
+cube.setPosition(-4,3,0);
 scene.add(cube);
 
 var ob1 = new Object();
 
 ob1.addGeometry(new THREE.SphereGeometry(4, 20, 20));
-ob1.addMaterial(new THREE.MeshBasicMaterial({
+ob1.addMaterial(new THREE.MeshLambertMaterial({
     color:
-        0x7777ff, wireframe: true
+    0x7777ff
 }));
 ob1.createMesh();
 
-ob1.setPosition(20,4,2);
+ob1.setPosition(20, 4, 2);
 
 
 scene.add(ob1);
-lighting();
+lighting(scene);
+
 camera.position.x = -30;
 camera.position.y = 40;
 camera.position.z = 30;
