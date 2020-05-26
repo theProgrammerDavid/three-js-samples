964cec5e6e6f2c3a93eaa9e8dad64fa9ec3f7413
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-26 12:11:01 +0530

    dev: lowered shadow map quality

--- src/lighting.js
+++ src/lighting.js
@@ -2,8 +2,8 @@ function init(scene) {
     var spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set(-40, 60, -10);
     spotLight.castShadow=true;
-    spotLight.shadowMapHeight=1024;
-    spotLight.shadowMapWidth=1024;
+    spotLight.shadowMapHeight=128*4;
+    spotLight.shadowMapWidth=128*4;
     scene.add(spotLight);
 }
 
