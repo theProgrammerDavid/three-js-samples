3730f058309f272fdd367b976b8dfd95f345b4c4
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-27 21:46:49 +0530

    fix: interfaced with GUI

--- src/lighting.js
+++ src/lighting.js
@@ -1,10 +1,11 @@
-function init(scene) {
+function lightingInit(scene) {
     var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.name = "light";
     spotLight.position.set(-40, 60, -10);
     spotLight.castShadow=true;
-    spotLight.shadowMapHeight=128*10;
-    spotLight.shadowMapWidth=128*10;
+    spotLight.shadowMapHeight=128*controls.lightMapMultiplier;
+    spotLight.shadowMapWidth=128*controls.lightMapMultiplier;
     scene.add(spotLight);
 }
 
-module.exports = init;
+module.exports = lightingInit;
