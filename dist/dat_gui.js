024a03db33fcd5c3e2f94f8a4c83f4597b49ca7a
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-29 23:37:49 +0530

    [WIP] GUI

--- dist/dat_gui.js
+++ dist/dat_gui.js
@@ -1,32 +1,38 @@
 var controls = new (function () {
-    this.rotationSpeed = 0.02;
-    this.bouncingSpeed = 0.03;
-    this.lightMapMultiplier = 6;
-    this.dynamicUpdate=false;
-  })();
+  this.rotationSpeed = 0.02;
+  this.bouncingSpeed = 0.03;
+  this.lightMapMultiplier = 6;
 
-  var gui = new dat.GUI();
-  var obj = {
-    updateShadow: function () {
-      updateLighting();
-    },
-  };
-  var info = {
-    info: function () {
-        window.open("https://github.com/theProgrammerDavid/three-js-samples/blob/master/README.md", '_blank');
-    },
-  };
+})();
 
-  gui.add(controls, "rotationSpeed", 0, 0.5);
-  gui.add(controls, "bouncingSpeed", 0, 0.5);
-  gui.add(controls, "lightMapMultiplier", 1, 15, 1);
-  gui.add(controls, "dynamicUpdate", true,false)
-  gui.add(obj, "updateShadow");
-  gui.add(info, "info");
+var gui = new dat.GUI();
 
-  function updateLighting() {
-    var light=scene.getObjectByName("light");
-    light.shadow.mapSize.set(128*controls.lightMapMultiplier, 128*controls.lightMapMultiplier);
-    light.shadow.map.dispose();
-    light.shadow.map=null;
+var info = {
+  info: function () {
+    window.open("https://github.com/theProgrammerDavid/three-js-samples/blob/master/README.md", '_blank');
+  },
+};
+
+var physics = {
+  addCubes: function () {
+
   }
+}
+gui.add(controls, "rotationSpeed", 0, 0.5);
+gui.add(controls, "bouncingSpeed", 0, 0.5);
+var shadowController = gui.add(controls, "lightMapMultiplier", 1, 15, 1);
+
+
+gui.add(physics, "addCubes");
+gui.add(info, "info");
+
+shadowController.onChange((value) => {
+  updateLighting();
+});
+
+function updateLighting() {
+  var light = scene.getObjectByName("light");
+  light.shadow.mapSize.set(128 * controls.lightMapMultiplier, 128 * controls.lightMapMultiplier);
+  light.shadow.map.dispose();
+  light.shadow.map = null;
+}
