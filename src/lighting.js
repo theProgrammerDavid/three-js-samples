96aeb2175cacfd7e756e987bd048968d4e866d56
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-30 00:29:05 +0530

    fix: GUI reference

--- src/lighting.js
+++ src/lighting.js
@@ -7,7 +7,7 @@ function lightingInit(scene) {
     spotLight.position.set(-40, 60, -10);
     spotLight.castShadow = true;
 
-    spotLight.shadow.mapSize.set(128 * GUI.controls.lightMapMultiplier, 128 * controls.lightMapMultiplier);
+    spotLight.shadow.mapSize.set(128 * GUI.controls.lightMapMultiplier, 128 * GUI. controls.lightMapMultiplier);
     scene.add(spotLight);
 }
 
